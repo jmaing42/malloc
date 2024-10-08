ea6affba104daeab45b336b5ad0dcacaea8852da
Juyeong Maing <mjy9088@naver.com>
2024-10-08 21:17:17 +0900

    add function prototypes

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,16 @@
+const std = @import("std");
+
 export fn malloc(size: usize) ?*u8 {
     _ = size;
     return null;
 }
+export fn free(ptr: ?*u8) void {
+    _ = ptr;
+}
+export fn realloc(ptr: ?*u8, size: usize) ?*u8 {
+    free(ptr);
+    _ = size;
+    return null;
+}
+export fn show_alloc_mem() void {}
+export fn show_alloc_mem_ex() void {}
